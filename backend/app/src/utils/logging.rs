653a79b42b1e9a94d06cc3d99bf8a7288ef41928
jb-alvarado <jb@pixelcrusher.de>
2026-03-17 09:12:59 +0100

    move log_middleware

--- backend/app/src/utils/logging.rs
+++ backend/app/src/utils/logging.rs
@@ -1,4 +1,15 @@
+use std::time::Instant;
+
+use axum::{
+    body::Body,
+    http::{
+        Request, Response,
+        header::{CONTENT_LENGTH, REFERER, USER_AGENT},
+    },
+    middleware::Next,
+};
 use chrono::Local;
+use tracing::{error, info, warn};
 use tracing_subscriber::{
     EnvFilter, Layer,
     fmt::{self, format::Writer, time::FormatTime},
@@ -51,4 +62,57 @@ pub fn init_tracing(level: Option<String>, timestamp: bool) {
         .with(filter)
         .with(fmt_layer)
         .init();
+}
+
+pub async fn log_middleware(req: Request<Body>, next: Next) -> Response<Body> {
+    let start = Instant::now();
+
+    let m = req.method().clone();
+    let uri = req.uri().clone();
+    let v = req.version();
+
+    let ip = req
+        .extensions()
+        .get::<real::RealIp>()
+        .map(|ip| ip.0.to_string())
+        .unwrap_or_else(|| "-".into());
+
+    let r = req
+        .headers()
+        .get(REFERER)
+        .and_then(|v| v.to_str().ok())
+        .unwrap_or("-")
+        .to_string();
+
+    let a = req
+        .headers()
+        .get(USER_AGENT)
+        .and_then(|v| v.to_str().ok())
+        .unwrap_or("-")
+        .to_string();
+
+    let res = next.run(req).await;
+
+    let status = res.status().as_u16();
+    let size = res
+        .headers()
+        .get(CONTENT_LENGTH)
+        .and_then(|v| v.to_str().ok())
+        .unwrap_or("-");
+
+    let l = start.elapsed().as_secs_f64();
+
+    match status {
+        500..=599 => {
+            error!(r#"{ip} "{m} {uri} {v:?}" {status} {size} "{r}" "{a}" {l:.6}"#);
+        }
+        401 | 403 | 429 => {
+            warn!(r#"{ip} "{m} {uri} {v:?}" {status} {size} "{r}" "{a}" {l:.6}"#);
+        }
+        _ => {
+            info!(r#"{ip} "{m} {uri} {v:?}" {status} {size} "{r}" "{a}" {l:.6}"#);
+        }
+    }
+
+    res
 }
