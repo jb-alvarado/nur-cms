a292784d227be124a31a6d3acef033d6174ff476
Jonathan Baecker <jonbae77@gmail.com>
2026-06-16 16:27:50 +0200

    add tsconfigRootDir

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -1,7 +1,10 @@
 import { globalIgnores } from 'eslint/config'
+import { fileURLToPath } from 'node:url'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+const tsconfigRootDir = fileURLToPath(new URL('.', import.meta.url))
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -12,6 +15,9 @@ export default defineConfigWithVueTs(
     {
         name: 'app/files-to-lint',
         files: ['**/*.{ts,mts,tsx,vue}'],
+        languageOptions: {
+            parserOptions: { tsconfigRootDir },
+        },
     },
 
     globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
@@ -26,5 +32,5 @@ export default defineConfigWithVueTs(
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/ban-ts-comment': 'off',
         },
-    }
+    },
 )
