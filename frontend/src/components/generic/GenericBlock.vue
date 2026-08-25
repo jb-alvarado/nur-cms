9618863dae6ca972727095e9df5bce1a58ad09bc
jb-alvarado <jb@pixelcrusher.de>
2026-08-25 09:10:59 +0200

    Support multiline text in generic blocks

--- frontend/src/components/generic/GenericBlock.vue
+++ frontend/src/components/generic/GenericBlock.vue
@@ -2,13 +2,18 @@
 import { type ModelRef } from 'vue'
 
 const block: ModelRef<Record<string, any> | undefined> = defineModel('block')
-
 </script>
 <template>
     <div v-if="block" class="flex flex-col gap-2">
-        <div v-for="(_, key) in block" :key="key" class="flex items-center gap-2">
-            <label class="min-w-20">{{ key }}: </label>
-            <input v-model="block[key]" type="text" class="input grow" />
+        <div
+            v-for="(_, key) in block"
+            :key="key"
+            class="flex gap-2"
+            :class="key === 'text' ? 'items-start' : 'items-center'"
+        >
+            <label class="min-w-20" :class="{ 'pt-3': key === 'text' }">{{ key }}: </label>
+            <textarea v-if="key === 'text'" v-model="block[key]" rows="3" class="textarea grow"></textarea>
+            <input v-else v-model="block[key]" type="text" class="input grow" />
         </div>
     </div>
 </template>
