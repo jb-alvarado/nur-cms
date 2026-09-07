592f3af4a4bd163f56aeccc1c2c618e5d6d7b3bd
Jonathan Baecker <jonbae77@gmail.com>
2026-09-07 13:50:43 +0200

    fix build path

--- backend/app/build.rs
+++ backend/app/build.rs
@@ -12,7 +12,7 @@ fn main() {
     {
         let output = Command::new("npm")
             .args(["run", "build"])
-            .current_dir("../../frontend")
+            .current_dir("../..")
             .stdout(Stdio::piped())
             .stderr(Stdio::inherit())
             .spawn()
